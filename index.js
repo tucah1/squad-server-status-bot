68d94c2a9aa3f21fd982d91b34f456d7ea177f56
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-03-20 13:03:38 +0100

    server players listing - not tested

--- index.js
+++ index.js
@@ -7,36 +7,42 @@ const TOKEN = process.env.TOKEN
 
 const getServerInfo = (server) => {
 	return new Promise(async (resolve, reject) => {
-		let ip = ''
-		let port = 27165
-		if (server === 'mixed') {
-			ip = '172.93.180.8'
-			port = 9010
-		} else if (server === 'occupation') {
-			ip = '172.93.180.9'
-			port = 9010
-		} else if (server === 'invasion') {
-			ip = '172.93.180.64'
-			port = 27165
-		} else if (server === 'laboratory') {
-			ip = '172.93.180.54'
-			port = 27165
-		} else if (server === 'event') {
-			ip = '172.93.180.6'
-			port = 9010
-		} else {
-			reject('server-name')
+		const serverTypes = {
+			mixed: {
+				ip: '172.93.180.8',
+				port: 9010,
+			},
+			occupation: {
+				ip: '172.93.180.9',
+				port: 9010,
+			},
+			invasion: {
+				ip: '172.93.180.64',
+				port: 27165,
+			},
+			laboratory: {
+				ip: '172.93.180.54',
+				port: 27165,
+			},
+			event: {
+				ip: '172.93.180.6',
+				port: 9010,
+			},
 		}
 
-		try {
-			let result = await Gamedig.query({
-				type: 'squad',
-				host: ip,
-				port,
-			})
-			resolve(result)
-		} catch (error) {
-			reject(error)
+		if (server in serverTypes) {
+			try {
+				let result = await Gamedig.query({
+					type: 'squad',
+					host: serverTypes[server].ip,
+					port: serverTypes[server].port,
+				})
+				resolve(result)
+			} catch (error) {
+				reject(error)
+			}
+		} else {
+			reject('server-name')
 		}
 	})
 }
@@ -70,6 +76,24 @@ bot.on('message', async (msg) => {
 			let helpMessage =
 				'To check server status please type in chat following command: !server-status [server-name]\nAvailable server names:\n- mixed\n- occupation\n- invasion\n- laboratory\n- event'
 			msg.channel.send(helpMessage)
+		} else if (tokens[0] === '!server-player-list' && tokens.length > 1) {
+			try {
+				let result = await getServerInfo(tokens[1])
+				let response = `${result.name}\n\n`
+				result.players.forEach((x) => {
+					response += `${x.name}\n`
+				})
+				msg.channel.send(response)
+			} catch (error) {
+				if (error === 'server-name') {
+					msg.channel.send(
+						'Invalid server name! Please checkout !server-status-help command.'
+					)
+				} else {
+					console.log(error)
+					msg.channel.send('Server is offline!')
+				}
+			}
 		} else {
 			msg.channel.send(
 				'Invalid command! Please checkout !server-status-help command.'
